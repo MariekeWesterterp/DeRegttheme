e95f82034512958f237795b7b51e6cc1ba2608ae
emestrom <93645933+emestrom@users.noreply.github.com>
2025-09-30 15:51:35 +0200

    some adjustments in styling and cards, faqs and product-block modules

--- DeRegt/modules/faqs.module/module.js
+++ DeRegt/modules/faqs.module/module.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   const items = document.querySelectorAll('.faqs__accordion-button')
-  console.log('Aantal knoppen gevonden:', items.length) // Controleer of de knoppen worden gevonden
+
 
   function toggleAccordion() {
     const itemToggle = this.getAttribute('aria-expanded')
-    console.log('Toggle status voor knop:', itemToggle) // Controleer de huidige status
+    console.log('Toggle status voor knop:', itemToggle) 
 
     for (let i = 0; i < items.length; i++) {
       items[i].setAttribute('aria-expanded', 'false')
